8b16cb3c092ea31e4004f68633b5342c3a8ac088
Gustavito18 <lmayorgao@miumg.edu.gt>
2022-02-27 22:16:43 -0600

    Division

---
+++ Division.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+int main ()
+{
+	int primero, segundo, operacion;
+	
+		
+	cout<<"Ingrese el numero a dividir"<<endl<<endl;
+	cout<<"Ingrese el primer numero:"<<endl;
+	cin>>primero;
+	cout<<"Ingrese el segundo numero:"<<endl;
+	cin>>segundo;
+	
+	operacion = primero / segundo;
+	cout<<"Su total es de:"<<operacion;
+	
+}
